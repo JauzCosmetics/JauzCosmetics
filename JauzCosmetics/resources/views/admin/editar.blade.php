97cb3bdaf843f30cd809ec4cfcb3b27bd861dff4
*Andresrvt* <*andresrvt@gmail.com*>
2023-02-06 13:58:53 +0100

    creacion users

--- JauzCosmetics/resources/views/admin/editar.blade.php
+++ JauzCosmetics/resources/views/admin/editar.blade.php
@@ -1,27 +1,28 @@
 @extends('template')
 
-@section ('editar')
+@section('editar')
+    <h2>Editando el producto {{ $product->id }}</h2>
 
-<h2>Editando el producto {{ $product -> id }}</h2> 
+    @if (session('mensaje'))
+        <div class="alert alert-success">{{ session('mensaje') }}</div>
+    @endif
 
-@if (session('mensaje')) 
-    <div class="alert alert-success">{{ session('mensaje')}}</div> 
-@endif 
+    <form action="{{ route('admin.actualizar', $product->id) }}" method="POST">
+        @method('PUT') {{-- Necesitamos cambiar al método PUT para editar --}}
+        @csrf {{-- Cláusula para obtener un token de formulario al enviarlo --}}
 
-<form action="{{ route('admin.actualizar', $product -> id) }}" method="POST"> 
-    @method('PUT') {{-- Necesitamos cambiar al método PUT para editar --}} 
-    @csrf {{-- Cláusula para obtener un token de formulario al enviarlo --}} 
+        @error('name')
+            <div class="alert alert-danger"> El nombre es obligatorio </div>
+        @enderror
 
-    @error('name') 
-        <div class="alert alert-danger"> El nombre es obligatorio </div> 
-    @enderror 
+        @error('description')
+            <div class="alert alert-danger"> La descripción es obligatoria </div>
+        @enderror
 
-    @error('description') 
-        <div class="alert alert-danger"> La descripción es obligatoria </div> 
-    @enderror 
-
-    <input type="text" name="name" class="form-control mb-2" value="{{ $product -> name }}" placeholder="Nombre del producto" autofocus > 
-    <input type="text" name="description" placeholder="Descripción del producto" class="form-control mb-2" value="{{ $product -> description }}" > 
-    <button class="btn btn-primary btn-block" type="submit">Guardar cambios</button> 
-</form>
+        <input type="text" name="name" class="form-control mb-2" value="{{ $product->name }}"
+            placeholder="Nombre del producto" autofocus>
+        <input type="text" name="description" placeholder="Descripción del producto" class="form-control mb-2"
+            value="{{ $product->description }}">
+        <button class="btn btn-primary btn-block" type="submit">Guardar cambios</button>
+    </form>
 @endsection
