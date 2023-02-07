187971e59e2b126ccb06280084d5b4bc902c6b99
*Andresrvt* <*andresrvt@gmail.com*>
2023-02-07 14:16:33 +0100

    estilos crear-editar

--- JauzCosmetics/resources/views/admin/crear.blade.php
+++ JauzCosmetics/resources/views/admin/crear.blade.php
@@ -3,42 +3,50 @@
 @section('crear')
     <div class="container py-5">
         <div class="row py-5">
-        <form class="col-md-9 m-auto" action="{{ route('admin.guardar') }}" method="POST">
-            <h1>Crear un producto</h1>
-            @csrf {{-- Cláusula para obtener un token de formulario al enviarlo --}}
-            <input type="text" name="name" placeholder="Nombre del producto" class="form-control mb-2" autofocus>
-            <input type="text" name="price" placeholder="Precio del producto" class="form-control mb-2">
-            <input type="text" name="stock" placeholder="Cantidad de unidades del producto" class="form-control mb-2">
-            <input type="text" name="description" placeholder="Descripción del producto" class="form-control mb-2">
-{{--             <label for="category">Elige una categoría</label>
+            <form class="col-md-9 m-auto" action="{{ route('admin.guardar') }}" method="POST">
+                <h1>Crear un producto</h1>
+                @csrf {{-- Cláusula para obtener un token de formulario al enviarlo --}}
+                <input type="text" name="name" placeholder="Nombre del producto" class="form-control mb-2" autofocus>
+                <input type="text" name="price" placeholder="Precio del producto" class="form-control mb-2">
+                <input type="text" name="stock" placeholder="Cantidad de unidades del producto"
+                    class="form-control mb-2">
+                <input type="text" name="description" placeholder="Descripción del producto" class="form-control mb-2">
+                {{--             <label for="category">Elige una categoría</label>
                 <select id="category" name="category">
                     <option value="maquillaje">Maquillaje</option>
                     <option value="accesorio">Accesorios</option>
                 </select>
             <input type="file" name="fotosProd[0]">
             <button type="submit">GUARDAR</button> --}}
-            <button class="btn btn-primary btn-block" type="submit">Enviar</button>
-            @if (session('mensaje'))
-                <div class="alert alert-success" role="alert">
-                    {{ session('mensaje') }}
-                </div>
-            @endif
+                <a href="admin">
+                    <button class="btn btn-primary btn-block" type="submit">Enviar</button>
+                </a>
 
-        @error('name')
-            <div class="alert alert-danger"> El nombre es obligatorio </div>
-        @enderror
+                @if (session('mensaje'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('mensaje') }}
+                    </div>
+                @endif
 
-        @error('price')
-            <div class="alert alert-danger"> El precio es obligatorio </div>
-        @enderror
+                @error('name')
+                    <div class="alert alert-danger"> El nombre es obligatorio </div>
+                @enderror
 
-        @error('stock')
-            <div class="alert alert-danger"> El stock es obligatorio </div>
-        @enderror
-        
-        @error('description')
-            <div class="alert alert-danger"> La descripción es obligatoria </div>
-        @enderror
+                @error('price')
+                    <div class="alert alert-danger"> El precio es obligatorio </div>
+                @enderror
+
+                @error('stock')
+                    <div class="alert alert-danger"> El stock es obligatorio </div>
+                @enderror
+
+                @error('description')
+                    <div class="alert alert-danger"> La descripción es obligatoria </div>
+                @enderror
+            </form>
+        </div>
+    </div>
+@endsection
 {{-- 
         @error('category')
             <div class="alert alert-danger"> La categoría es obligatoria </div>
@@ -47,6 +55,3 @@
         @error('fotosProd')
             <div class="alert alert-danger"> Las imágenes es obligatoria </div>
         @enderror --}}
-@endsection
-
-    
--- JauzCosmetics/resources/views/admin/editar.blade.php
+++ JauzCosmetics/resources/views/admin/editar.blade.php
@@ -1,43 +1,55 @@
 @extends('template')
 
 @section('editar')
-    <h2>Editando el producto {{ $product->id }}</h2>
+<div class="container py-5 col-2">
+    <div class="row py-5 text-center">
+        <h2 class="mb-5">Editando el producto {{ $product->id }}</h2>
 
-    @if (session('mensaje'))
-        <div class="alert alert-success">{{ session('mensaje') }}</div>
-    @endif
-    <form action="{{ route('admin.actualizar', $product-> id) }}" method="POST">
-        @method('PUT') {{-- Necesitamos cambiar al método PUT para editar --}}
-        @csrf {{-- Cláusula para obtener un token de formulario al enviarlo --}}
-
-        @error('name')
-            <div class="alert alert-danger"> El nombre es obligatorio </div>
-        @enderror
-
-        @error('price')
-            <div class="alert alert-danger"> El precio es obligatorio </div>
-        @enderror
-
-        @error('stock')
-            <div class="alert alert-danger"> El stock es obligatorio </div>
-        @enderror
-        
-        @error('description')
-            <div class="alert alert-danger"> La descripción es obligatoria </div>
-        @enderror
-
-        @error('category')
-            <div class="alert alert-danger"> La categoría es obligatoria </div>
-        @enderror
-
-        @error('fotosProd')
-            <div class="alert alert-danger"> Las imágenes es obligatoria </div>
-        @enderror
-
-        <input type="text" name="name" class="form-control mb-2" value="{{ $product-> name }}"
-            placeholder="Nombre del producto" autofocus>
-        <input type="text" name="description" placeholder="Descripción del producto" class="form-control mb-2"
+        @if (session('mensaje'))
+            <div class="alert alert-success">{{ session('mensaje') }}</div>
+        @endif
+    
+            <input type="text" name="price" placeholder="Descripción del producto" class="form-control mb-2"
+            value="{{ $product-> price }}">
+    
+            <input type="text" name="stock" placeholder="Descripción del producto" class="form-control mb-2"
+            value="{{ $product-> stock }}">
+    
+            <input type="text" name="description" placeholder="Descripción del producto" class="form-control mb-2"
             value="{{ $product-> description }}">
-        <button class="btn btn-primary btn-block" type="submit">Guardar cambios</button>
-    </form>
+    
+            <button class="btn btn-primary btn-block mt-5" type="submit">Guardar cambios</button>
+    
+            @error('name')
+                <div class="alert alert-danger"> El nombre es obligatorio </div>
+            @enderror
+    
+            @error('price')
+                <div class="alert alert-danger"> El precio es obligatorio </div>
+            @enderror
+    
+            @error('stock')
+                <div class="alert alert-danger"> El stock es obligatorio </div>
+            @enderror
+            
+            @error('description')
+                <div class="alert alert-danger"> La descripción es obligatoria </div>
+            @enderror
+    
+            @error('category')
+                <div class="alert alert-danger"> La categoría es obligatoria </div>
+            @enderror
+    
+            @error('fotosProd')
+                <div class="alert alert-danger"> Las imágenes es obligatoria </div>
+            @enderror
+        </form>   
+    </div>
+</div>
+
+
+
+
+
+    
 @endsection
