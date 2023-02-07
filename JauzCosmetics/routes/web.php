001e71345eb3345de37638c650438f0ba1058107
Javier Antequera <javiantmine@gmail.com>
2023-02-07 10:28:14 +0100

    avances

--- JauzCosmetics/routes/web.php
+++ JauzCosmetics/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\ShopController;
 
 /*
@@ -39,8 +40,10 @@ Route::get('/login', function () {
 })->name('login');
 
 Route::get('/register', function () {
-    return view('register');
+    return view('auth/register');
 })->name('register');
+
+Route::post('/register',[RegisterController::class,'register']);
 
 Route::get('/maquillaje', [ShopController::class, 'maquillaje'])->name('maquillaje');
 Route::get('/accesorio', [ShopController::class, 'accesorio'])->name('accesorio');
