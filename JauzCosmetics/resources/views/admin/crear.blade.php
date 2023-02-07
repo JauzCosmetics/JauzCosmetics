bd2518fcd2d96f3ae12c98d6ac845a6a412fab36
*Andresrvt* <*andresrvt@gmail.com*>
2023-02-07 12:47:33 +0100

    crear y editar funcional

--- JauzCosmetics/resources/views/admin/crear.blade.php
+++ JauzCosmetics/resources/views/admin/crear.blade.php
@@ -10,22 +10,23 @@
             <input type="text" name="price" placeholder="Precio del producto" class="form-control mb-2">
             <input type="text" name="stock" placeholder="Cantidad de unidades del producto" class="form-control mb-2">
             <input type="text" name="description" placeholder="Descripción del producto" class="form-control mb-2">
-            <label for="category">Elige una categoría</label>
+{{--             <label for="category">Elige una categoría</label>
                 <select id="category" name="category">
                     <option value="maquillaje">Maquillaje</option>
                     <option value="accesorio">Accesorios</option>
                 </select>
             <input type="file" name="fotosProd[0]">
-            <button type="submit">GUARDAR</button>
+            <button type="submit">GUARDAR</button> --}}
             <button class="btn btn-primary btn-block" type="submit">Enviar</button>
             @if (session('mensaje'))
                 <div class="alert alert-success" role="alert">
                     {{ session('mensaje') }}
                 </div>
             @endif
-            @error('name')
-                <div class="alert alert-danger"> El nombre es obligatorio </div>
-            @enderror
+
+        @error('name')
+            <div class="alert alert-danger"> El nombre es obligatorio </div>
+        @enderror
 
         @error('price')
             <div class="alert alert-danger"> El precio es obligatorio </div>
@@ -38,14 +39,14 @@
         @error('description')
             <div class="alert alert-danger"> La descripción es obligatoria </div>
         @enderror
-
+{{-- 
         @error('category')
             <div class="alert alert-danger"> La categoría es obligatoria </div>
         @enderror
 
         @error('fotosProd')
             <div class="alert alert-danger"> Las imágenes es obligatoria </div>
-        @enderror
-        @endsection
+        @enderror --}}
+@endsection
 
     
