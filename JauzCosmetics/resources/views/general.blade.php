5e74c5e3e492b1dc826b01df1393bd9249603cb4
Javier Antequera <javiantmine@gmail.com>
2023-02-10 12:34:51 +0100

    avances

--- JauzCosmetics/resources/views/general.blade.php
+++ JauzCosmetics/resources/views/general.blade.php
@@ -3,22 +3,27 @@
 <html lang="en">
     @yield('head')
 <body>
-    @yield('nav')
-    @yield('modal')
-    @yield('carrousel')
-    @yield('novedades')
-    @yield('bestSeller')
-    @yield('article')
-    @yield('crear')
-    @yield('detalle')
-    @yield('editar')
-    @yield('products')
-    @yield('contact')
-    @yield('adminTable')
-    @yield('about')
-    @yield('cart')
-    @yield('login')
-    @yield('register')
-    @yield('footer')
+    {{-- <div id="loader" class="spinner-border text-dark" role="status">
+        <span class="sr-only">Loading...</span>
+    </div>
+    <div id="all" hidden> --}}
+        @yield('nav')
+        @yield('modal')
+        @yield('carrousel')
+        @yield('novedades')
+        @yield('bestSeller')
+        @yield('article')
+        @yield('crear')
+        @yield('detalle')
+        @yield('editar')
+        @yield('products')
+        @yield('contact')
+        @yield('adminTable')
+        @yield('about')
+        @yield('cart')
+        @yield('login')
+        @yield('register')
+        @yield('footer')
+    {{-- </div> --}}
 </body>
 </html>
