0bf8e8e81fa02d04d37987b233ac601c91277b50
Javier Antequera <javiantmine@gmail.com>
2023-02-23 13:41:42 +0100

    botones color

--- JauzCosmetics/resources/views/auth/register.blade.php
+++ JauzCosmetics/resources/views/auth/register.blade.php
@@ -63,7 +63,7 @@
 
 
                                         <div class="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                                            <button type="submit" class="btn btn-primary">
+                                            <button type="submit" class="btn bgpropio text-white">
                                                 {{ __('Register') }}
                                             </button>
                                         </div>
